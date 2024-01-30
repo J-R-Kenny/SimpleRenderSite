5b336b3113a0b8492479eb62c995574d2fbf89b3
J-R-Kenny <153197242+J-R-Kenny@users.noreply.github.com>
2024-01-30 14:57:42 +0000

    Added debugging console logs

--- views/app.js
+++ views/app.js
@@ -1,41 +1,53 @@
+console.log(`Start of app.js`);
+
 const express = require("express");
 const app = express();
 const { Pool } = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
+console.log(`About to set the pool connection`);
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
+console.log(`App listen incoming`);
 app.listen(3000);
 
+console.log(`Setting View Engine`);
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
+  console.log(`Inside .get for /`);
   res.render("index");
 });
 
 app.get("/jokes", async (req, res) => {
+  console.log(`Inside .get for /jokes`);
   const jokes = await mainTask();
   res.render("jokes", { jokes });
 });
 
 app.get("/rolldie", (req, res) => {
+  console.log(`Inside .get for /rolldie`);
   res.render("rolldie", { diceRoll, randomSentence, returnRandomOneToFive });
 });
 
 app.get("/warhammer", (req, res) => {
+  console.log(`Inside .get for /warhammer`);
   res.render("warhammer", { factions });
 });
 
 app.use((req, res) => {
+  console.log(`Inside .get for 404`);
   res.status(404).render("404");
 });
 
 const diceRoll = function rollDice() {
+  console.log(`Inside diceRoll Function`);
   return Math.floor(Math.random() * 6) + 1;
 };
 
 function randomSentence() {
+  console.log(`Inside randomSentence function`);
   const subjects = ["cat", "dog", "tree", "house", "car"];
   const verbs = ["runs", "jumps", "sleeps", "eats", "drives"];
   const adjectives = ["happy", "big", "green", "fast", "loud"];
@@ -45,6 +57,7 @@ function randomSentence() {
 }
 
 function returnRandomOneToFive() {
+  console.log(`Inside returnRandom Function`);
   return Math.floor(Math.random() * 5);
 }
 
@@ -70,6 +83,7 @@ const factions = [
 ];
 
 async function mainTask() {
+  console.log(`Inside mainTask DB Query Function`);
   const dbResult = await pool.query("select * from jokes");
   return dbResult.rows;
 }
